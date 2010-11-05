2d8699851b0e66c47aeb61491ec480fe75508834
Rod Paddock <rodneypaddock@gmail.com>
2010-11-05 01:04:20 -0500

    Initial Commit

---
+++ SilverlightCalculator/Calculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Browser;
+
+namespace SilverlightCalculator
+{
+    public class Calculator
+    {
+        [ScriptableMember]
+        public string Multiply(string value1, string value2)
+        {
+            var decimal_1 = System.Convert.ToDecimal(value1);
+            var decimal_2 = System.Convert.ToDecimal(value2);
+            return (decimal_1*decimal_2).ToString();
+        }
+
+        [ScriptableMember]
+        public string Add(string value1, string value2)
+        {
+            var decimal_1 = System.Convert.ToDecimal(value1);
+            var decimal_2 = System.Convert.ToDecimal(value2);
+            return (decimal_1 + decimal_2).ToString();
+        }
+
+        [ScriptableMember]
+        public string Divide(string value1, string value2)
+        {
+            var decimal_1 = System.Convert.ToDecimal(value1);
+
+            var decimal_2 = System.Convert.ToDecimal(value2);
+
+            return (decimal_1/decimal_2).ToString();
+        }
+
+        [
+            ScriptableMember]
+        public string Subtract(string value1, string value2)
+        {
+            var decimal_1 = System.Convert.ToDecimal(value1);
+
+            var decimal_2 = System.Convert.ToDecimal(value2);
+
+            return (decimal_1 - decimal_2).ToString();
+        }
+    }
+}
